36e7c8cd9251a24f93779586a4741a81aa86be10
gmittal <gautam@mittal.net>
2016-07-08 16:33:20 -0700

    refactoring UI

---
+++ request/js/main.js
@@ -0,0 +1,35 @@
+
+$(function() {
+    
+
+
+    // What happens when the submit button is clicked
+    $('#submit').click(function() {
+      var d = {
+        "name": $(".contact input[name=name]").val(),
+        "age": $("form input[name=age]").val(),
+        "gender": $("form select[name=gender]").val(),
+        "contactMethod": $("form select[name=preferredComm]").val(),
+        "contact": $("form input[name=contact]").val(),
+        "situation": $("form textarea[name=situation]").val()
+      };
+
+      $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/api/addUserRequest",
+        data: d,
+        error: function(err) { // I have no clue why, but the response gets passed through the error method
+          if (err.status == 200) {
+            $("#message").html('<span style="color:green;">Successfully submitted.</span>');
+            $(".contact")[0].reset();
+          } else {
+            console.log(err);
+            $("#message").html('<span style="color:red;">There was an error submitting.</span>');
+          }
+        },
+        dataType: 'json',
+      });
+
+      return false;
+    });
+  });
