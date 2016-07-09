49a986dcb9588ceb26c087c44023457bc7fb8d56
gmittal <gautam@mittal.net>
2016-07-08 18:14:48 -0700

    more questions, fixes, and better setup for referrals, etc.

--- request/js/main.js
+++ request/js/main.js
@@ -1,31 +1,71 @@
 $(function() {
-    var questionList = [
-      {"value": "Name", "helper": "What's your name?"},
-      {"value": "Age", "helper": "How old are you?"},
-      {"value": "Gender", "helper": "What gender do you identify as?"},
-      {"value": "School Name", "helper": "What school do you attend?"}
-      // {"value": "Referral", }
+    var referralQuestionList = [
+      {"key":"name", "value": "Their Name", "helper": "What's their name?"},
+      {"key":"referer_name", "value": "Your Name", "helper": "What's your name?"},
+      {"key":"age", "value": "Their Age", "helper": "How old are they?"},
+      {"key": "gender", "value": "Gender", "helper": "What gender do they identify as?"},
+      {"key": "school", "value": "School Name", "helper": "What school do they attend?"},
+      {"key": "contact", "value": "Their Contact", "helper": "What's the best way to reach them?"},
+      {"key": "referer_contact", "value": "Your Contact", "helper": "What's the best way to reach you?"},
+      {"key": "situation", "value": "Please Explain", "helper": "Please provide any additional information."}
+    ]
+    var individualQuestionList = [
+      {"key":"name", "value": "Your Name", "helper": "What's your name?"},
+      {"key":"age", "value": "Your Age", "helper": "How old are you?"},
+      {"key": "gender", "value": "Gender", "helper": "What gender do you identify as?"},
+      {"key": "school", "value": "School Name", "helper": "What school do you attend?"},
+      {"key": "contact", "value": "Your Contact", "helper": "What's the best way to reach you?"},
+      {"key": "situation", "value": "Please Explain", "helper": "What thoughts are you having?"} // definitely rephrase
     ];
+
     var helper = $("#helper");
     var mainInput = $("#mainField");
+    var questionList = individualQuestionList;
     var placeholder = mainInput.attr("placeholder");
     var inputJSON = {};
-    var currentQuestion = 0;
+    var currentQuestion = -1;
+    var referral = false;
+
+    // Process current question and pull up next question
+    function next() {
+      var input = mainInput.val();
+
+      if (currentQuestion == -1 && (input.toLowerCase() == "yes" || input.toLowerCase() == "y")) {
+        referral = true;
+        questionList = referralQuestionList;
+      }
+
+      inputJSON[currentQuestion !== -1 ? questionList[currentQuestion].key : "referral"] = currentQuestion !== -1 ? input : referral;
+
+      if (currentQuestion < questionList.length-1) {
+        currentQuestion++;
+        helper.fadeOut(function() {
+          helper.text(questionList[currentQuestion].helper);
+          mainInput.val("").attr("placeholder", questionList[currentQuestion].value);
+        }).fadeIn();
+      } else {
+        console.log(inputJSON);
+        helper.text("Thank you. We will be in touch shortly.");
+        mainInput.val("");
+      }
+    }
+
+
+
+
     $('.contact input').keyup(function(e){
       if (e.keyCode == 13 && mainInput.val().length !== 0) {
-          var input = mainInput.val();
-          inputJSON[placeholder] == input;
-          currentQuestion++;
-          helper.fadeOut(function() {
-            helper.text(questionList[currentQuestion].helper);
-            mainInput.val("").attr("placeholder", questionList[currentQuestion].value);
-          }).fadeIn();
+        next();
       }
     });
 
     $("#mainFieldSubmit").click(function() {
-
+      if (mainInput.val().length !== 0) {
+        next();
+      }
     });
+
+
 
     // What happens when the submit button is clicked
     $('#submit').click(function() {
