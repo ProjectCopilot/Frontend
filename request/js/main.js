6acd6bb22e3fddb8ba816c80f1b48e7fe6faac11
gmittal <gautam@mittal.net>
2016-07-08 18:50:01 -0700

    comments

--- request/js/main.js
+++ request/js/main.js
@@ -1,4 +1,7 @@
 $(function() {
+
+    // QUESTIONS (by default the form starts by asking whether the request is a referral or not)
+    // Will most likely need to be moved to a JSON file at some point in the near future
     var referralQuestionList = [
       {"key":"name", "value": "Their Name", "helper": "What's their name?"},
       {"key":"referer_name", "value": "Your Name", "helper": "What's your name?"},
@@ -18,6 +21,8 @@ $(function() {
       {"key": "situation", "value": "Please Explain", "helper": "What thoughts are you having?"} // definitely rephrase
     ];
 
+
+    // initialize standard form variables on page load
     var helper = $("#helper");
     var mainInput = $("#mainField");
     var questionList = individualQuestionList;
@@ -26,10 +31,12 @@ $(function() {
     var currentQuestion = -1;
     var referral = false;
 
+
     // Process current question and pull up next question
     function next() {
-      var input = mainInput.val();
+      var input = mainInput.val(); // grab main textfield input
 
+      // decide whether the request is a referral or not
       if (currentQuestion == -1 && (input.toLowerCase() == "yes" || input.toLowerCase() == "y")) {
         referral = true;
         questionList = referralQuestionList;
@@ -37,6 +44,7 @@ $(function() {
 
       inputJSON[currentQuestion !== -1 ? questionList[currentQuestion].key : "referral"] = currentQuestion !== -1 ? input : referral;
 
+      // iteratively move through all of the questions
       if (currentQuestion < questionList.length-1) {
         currentQuestion++;
         helper.fadeOut(function() {
@@ -44,6 +52,8 @@ $(function() {
           mainInput.val("").attr("placeholder", questionList[currentQuestion].value);
         }).fadeIn();
       } else {
+
+        // once all of the questions have been completed, show the SUBMIT button
         helper.text("Hit \"Finish\" to complete.");
         mainInput.val("").hide();
         $("#mainFieldSubmit").hide();
@@ -54,7 +64,7 @@ $(function() {
 
 
 
-
+    // Standard handlers for when the user hits return or "OK"
     $('.contact input').keyup(function(e){
       if (e.keyCode == 13 && mainInput.val().length !== 0) {
         next();
@@ -69,23 +79,24 @@ $(function() {
 
 
 
-    // What happens when the submit button is clicked
+    // SUBMIT button is clicked: Sends form data off to the server.
     $('#submit').click(function() {
       console.log(inputJSON);
 
+      // make the call
       $.ajax({
         type: "POST",
         url: "http://localhost:3000/api/addUserRequest",
         data: inputJSON,
-        error: function(err) { // I have no clue why, but the response gets passed through the error method
-          if (err.status == 200) {
+        error: function(err) { // Not sure why, but the response always gets passed through the error method (even if it was successful)
+          if (err.status == 200) { // if everything's all good, then fade everything out and redirect to the beginning of the form
             helper.text("Successfully submitted.");
             setTimeout(function() {
                 $("body").fadeOut(function() {
                   location.href = "/request";
                 });
             }, 1000);
-          } else {
+          } else { // something bad happened
             console.log(err);
             helper.html("There was an error submitting. Try again later.");
           }
