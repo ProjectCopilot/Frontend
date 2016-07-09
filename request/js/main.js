214807f73f66ef292e1892f5811ab3ddc334a4ac
gmittal <gautam@mittal.net>
2016-07-08 17:07:17 -0700

    added easy way to add new questions to UI

--- request/js/main.js
+++ request/js/main.js
@@ -1,12 +1,25 @@
 $(function() {
+    var questionList = [
+      {"value": "Name", "helper": "What's your name?"},
+      {"value": "Age", "helper": "How old are you?"},
+      {"value": "Gender", "helper": "What gender do you identify as?"},
+      {"value": "School Name", "helper": "What school do you attend?"}
+      // {"value": "Referral", }
+    ];
     var helper = $("#helper");
     var mainInput = $("#mainField");
     var placeholder = mainInput.attr("placeholder");
-
+    var inputJSON = {};
+    var currentQuestion = 0;
     $('.contact input').keyup(function(e){
-      if (e.keyCode == 13) {
+      if (e.keyCode == 13 && mainInput.val().length !== 0) {
           var input = mainInput.val();
-      
+          inputJSON[placeholder] == input;
+          currentQuestion++;
+          helper.fadeOut(function() {
+            helper.text(questionList[currentQuestion].helper);
+            mainInput.val("").attr("placeholder", questionList[currentQuestion].value);
+          }).fadeIn();
       }
     });
 
