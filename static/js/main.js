d8b95eedbeb64c6ed0b0c65d548ec80fe675f3be
gmittal <gautam@mittal.net>
2016-07-13 15:24:51 -0700

    fixed issue with textarea, working on cleaning it up

--- static/js/main.js
+++ static/js/main.js
@@ -26,10 +26,10 @@
           {"key": "gender", "type": "option", "options": ["Female", "Male", "Non-binary"], "value": "Gender", "helper": "What gender do they identify as?", "followUpValue": "NONE"},
           {"key": "school", "type": "option", "options": ["Henry M. Gunn High School", "Palo Alto High School"], "value": "School Name", "helper": "What school do they attend?", "followUpValue": "NONE"},
           {"key": "contactMethod", "type": "option", "options": ["SMS", "Email"], "value": "Preferred Contact", "helper": "What's the best way to reach them?", "followUpValue": "NONE"},
-          {"key": "contact", "value": "Their Contact", "helper": "Please provide their primary contact information.", "followUpValue": "NONE"},
+          {"key": "contact", "type": "text", "value": "Their Contact", "helper": "Please provide their primary contact information.", "followUpValue": "NONE"},
           {"key": "referer_contactMethod", "type": "option", "options": ["SMS", "Email"], "value": "Preferred Contact", "helper": "What's the best way to reach you?", "followUpValue": "NONE"},
-          {"key": "referer_contact", "value": "Your Contact", "helper": "Please provide your primary contact information.", "followUpValue": "NONE"},
-          {"key": "situation", "value": "Please Explain", "helper": "Please provide any additional information.", "followUpValue": "NONE"}
+          {"key": "referer_contact", "type": "text", "value": "Your Contact", "helper": "Please provide your primary contact information.", "followUpValue": "NONE"},
+          {"key": "situation", "type": "textarea", "value": "Please Explain", "helper": "Please provide any additional information.", "followUpValue": "NONE"}
         ]
       },
       {"key":"name", "type": "text", "value": "Your Name", "helper": "What's your name?", "followUpValue": "NONE"},
@@ -38,15 +38,17 @@
       {"key": "school", "type": "option", "options": ["Henry M. Gunn High School", "Palo Alto High School"], "value": "School Name", "helper": "What school do you attend?", "followUpValue": "NONE"},
       {"key": "contactMethod", "type": "option", "options": ["SMS", "Email"], "value": "Preferred Contact", "helper": "What's the best way to reach you?", "followUpValue": "NONE"},
       {"key": "contact", "type": "text", "value": "Your Contact", "helper": "Please provide your primary contact information.", "followUpValue": "NONE"},
-      {"key": "situation", "value": "Please Explain", "helper": "What thoughts are you having?", "followUpValue": "NONE"}
+      {"key": "situation", "type": "textarea", "value": "Please Explain", "helper": "What thoughts are you having?", "followUpValue": "NONE"}
     ];
+
 
 
     // initialize standard form variables on page load
     var helper = $("#helper");
     var mainInput = [
         $("#mainField"),
-        $("#mainOption")
+        $("#mainOption"),
+        $("#mainTextArea")
     ];
     var inputJSON = {};
     var questionQueue = [];
@@ -55,34 +57,54 @@
     }
     var currentQuestion = 0;
     var queueLength = questionQueue.length;
-    var ix = questionQueue[currentQuestion].type == "option" ? 1 : 0;
-    var referral = false;
+    var ix = getInputIndex(questionQueue[currentQuestion].type);
+
+
+    // return ix given type
+    function getInputIndex(type) {
+      if (type === "option") {
+        return 1;
+      } else if (type === "textarea") {
+        return 2;
+      } else {
+        return 0;
+      }
+    }
+
 
     // Process current question and pull up next question
     function next() {
 
       helper.fadeOut(function() {
-        if (currentQuestion < queueLength-1) {
+        if (currentQuestion < queueLength) {
           helper.text(questionQueue[currentQuestion].helper);
           // Is the question type an option or a textfield?
-          ix = questionQueue[currentQuestion].type == "option" ? 1 : 0;
-
+          ix = getInputIndex(questionQueue[currentQuestion].type);
+          console.log(currentQuestion, questionQueue[currentQuestion].type, ix);
           queueLength = questionQueue.length;
 
           if (ix == 1) {
             mainInput[0].css("display", "none");
-            mainInput[1].css("display", "inline-block"); // show the element
+            mainInput[1].css("display", "inline-block");
+            mainInput[2].css("display", "none");
             $("#mainOption").html("<option value=\"\" id=\"optionHelper\" disabled selected>Option Placeholder</option>");
             $("#optionHelper").text(questionQueue[currentQuestion].value);
             for (var i = 0; i < questionQueue[currentQuestion].options.length; i++) {
               mainInput[ix].append('<option value="'+questionQueue[currentQuestion].options[i]+'">'+questionQueue[currentQuestion].options[i]+'</option>');
             }
-          } else {
+          } else if (ix == 0) {
             mainInput[0].css("display", "inline-block");
             mainInput[1].css("display", "none");
+            mainInput[2].css("display", "none");
             mainInput[ix].val("").attr("placeholder", questionQueue[currentQuestion].value);
 
+          } else if (ix == 2) {
+            mainInput[2].css("display", "inline-block");
+            mainInput[0].css("display", "none");
+            mainInput[1].css("display", "none");
+            mainInput[ix].val("").attr("placeholder", questionQueue[currentQuestion].value);
           }
+
         } else {
           helper.text("Hit \"Finish\" to complete.");
           mainInput[ix].val("").hide();
@@ -95,24 +117,23 @@
 
       var input = mainInput[ix].val();
 
-
+      // add data to inputJSON, the object that will eventually be sent up to the server
       inputJSON[questionQueue[currentQuestion].key] = input;
 
       // iteratively move through all of the questions
+      if (mainInput[ix].val() == questionQueue[currentQuestion].followUpValue && questionQueue[currentQuestion].followUpValue !== "NONE") {
 
-        if (mainInput[ix].val() == questionQueue[currentQuestion].followUpValue && questionQueue[currentQuestion].followUpValue !== "NONE") {
-
-          var followUpArray = questionQueue[currentQuestion].followUpQuestions;
-          questionQueue.length = 0; // wipe array
-          for (var k = 0; k < followUpArray.length; k++) {
-              questionQueue.push(followUpArray[k]);
-          }
-
-          currentQuestion = 0;
-
-        } else {
-          if (mainInput[1].val() !== null || mainInput[0].val() !== "") currentQuestion++;
+        var followUpArray = questionQueue[currentQuestion].followUpQuestions;
+        questionQueue.length = 0; // wipe array
+        for (var k = 0; k < followUpArray.length; k++) {
+            questionQueue.push(followUpArray[k]);
         }
+
+        currentQuestion = 0;
+
+      } else {
+        if (mainInput[1].val() !== null || mainInput[0].val() !== "") currentQuestion++;
+      }
 
 
     }
@@ -124,7 +145,6 @@
 
     // Standard handlers for when the user hits return or "OK"
     $('.contact input').keyup(function(e){
-      // var ix = questionQueue[currentQuestion].type == "option" ? 1 : 0;
       if (e.keyCode == 13 && mainInput[ix].val().length !== 0) {
         next();
       }
