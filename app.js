5e71a8b296461ed9ed753c144be92f0f423f76ed
gmittal <gautam@mittal.net>
2016-07-18 13:52:19 -0700

    Removed env.js for security reasons

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 var express = require('express');
 var app = express();
 var dotenv = require('dotenv').config({path: __dirname+'/.env'});
+var fs = require('fs');
+
+app.get("/", function (req, res) {
+  res.send("YO");
+});
 
 app.use('/', express.static(__dirname+'/static'));
 
@@ -11,4 +16,3 @@ app.listen(process.env.PORT, process.env.HOSTNAME, function () {
   console.log('Copilot Concierge service successfully running at '
       + process.env.HOSTNAME + ':' + process.env.PORT);
 });
-
