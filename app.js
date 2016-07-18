e5bc6f4bec29bc9ce54f1042dd19b7fdd1bfd2fd
gmittal <gautam@mittal.net>
2016-07-18 14:01:07 -0700

    Successfully substituted in env variables in main.js

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ var app = express();
 var dotenv = require('dotenv').config({path: __dirname+'/.env'});
 var fs = require('fs');
 
-app.get("/", function (req, res) {
-  res.send("YO");
+// Substitute special environment variables
+app.get("/js/main.js", function (req, res) {
+  res.header("Content-Type",'application/javascript');
+
+  fs.readFile(__dirname+"/static/js/main.js", "utf-8", function (e, data) {
+    if (e) throw e;
+
+    var load = data.replace(/{HOSTNAME}/g, process.env.MAILROOM_HOSTNAME).replace(/{PORT}/g, process.env.MAILROOM_PORT);
+    res.send(load);
+  });
 });
 
 app.use('/', express.static(__dirname+'/static'));
