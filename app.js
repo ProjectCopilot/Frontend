636b51291930f252bab631d497163c5c644e75eb
Gautam Mittal <gautam@mittal.net>
2016-07-28 16:30:16 -0700

    Change startup text color (#50)
    * Add npm colors dependency to package.json
    
    * Changed server startup text color

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var colors = require('colors');
 var dotenv = require('dotenv').config({path: __dirname+'/.env'});
 var fs = require('fs');
 
@@ -21,6 +22,5 @@ app.use('/', express.static(__dirname+'/static'));
 
 
 app.listen(process.env.PORT, process.env.HOSTNAME, function () {
-  console.log('Copilot Concierge service successfully running at '
-      + process.env.HOSTNAME + ':' + process.env.PORT);
+  console.log(('Copilot Concierge running at ').blue + (process.env.HOSTNAME+":"+process.env.PORT).magenta);
 });
