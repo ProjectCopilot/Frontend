96dabc16f3696c31eb6f90ac1ac06a1d6fce665f
Ankit Ranjan <me@ankit.io>
2016-07-31 01:15:25 -0700

    Installed and began ESLint

--- static/js/validate.js
+++ static/js/validate.js
@@ -2,25 +2,25 @@
 // Set of validation functions to help parse and clean up form input
 // Copyright 2016 Project Copilot
 
-var validate = {
-  email: function(mail) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const validate = {
+  email(mail) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(mail);
   },
-  phone: function(phone) {
+  phone(phone) {
     try {
-      return phoneUtils.isValidNumber(phone, "US");
+      return phoneUtils.isValidNumber(phone, 'US');
     } catch (err) {
       return false;
     }
   },
-  contact: function(type, c) {
-    return type.toLowerCase() == "sms" ? validate["phone"](c) : validate["email"](c);
+  contact(type, c) {
+    return type.toLowerCase() == 'sms' ? validate['phone'](c) : validate['email'](c);
   },
-  string: function(s) {
-    return typeof s == "string" && s.length !== 0 ? true : false;
+  string(s) {
+    return typeof s == 'string' && s.length !== 0 ? true : false;
   },
-  number: function(n) {
+  number(n) {
     return isNaN(parseInt(n, 10)) ? false : true;
-  }
-}
+  },
+};
